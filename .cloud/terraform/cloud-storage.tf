6aac3d9357e69d946c0cd2e075f4504b12e82eaa
Roelof Roos <github@roelof.io>
2021-01-17 17:49:10 +0100

    Terraform - Added config
    Supports
    
     - Cloud Run our image
     - Cloud SQL for the data
    
    Configuration is stored in the Terraform Cloud

---
+++ .cloud/terraform/cloud-storage.tf
@@ -0,0 +1,23 @@
+resource "google_storage_bucket" "site_object_cache" {
+  name          = "${var.app_prefix}-app-storage"
+  location      = var.region
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  # Allow cross-origin requests
+  cors {
+    origin = ["e-voting.gumbo-millennium.nl"]
+    method = ["GET"]
+  }
+
+  # Purge files over 14 days old
+  lifecycle_rule {
+    condition {
+      age = 14
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
---
+++ .cloud/terraform/terraform.example.tfvars
@@ -0,0 +1,11 @@
+# Google Cloud credentials
+# Should have the Project:Editor permission
+credentials_file = ""
+
+# Your SQL login data
+cloud_sql_database = "laravel"
+cloud_sql_username = "laravel"
+cloud_sql_password = "laravel"
+
+# Application name
+app_prefix = "evoting2021"
